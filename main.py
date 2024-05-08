d55e617dc373c938953daf851666963ada1cbb5d
noam rachamim <noamra34@gmail.com>
2024-05-08 14:13:54 +0300

    new main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/hello/<name>')
 def hello_name(name):
-return 'Hello %s!' % name
+    return 'Hello %s!' % name
 
 if __name__ == '__main__':
-app.run()
+    app.run()
 
