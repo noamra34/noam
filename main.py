d626b3157a4628309ea7bf82546ebc22f37cdb96
noam rachamim <noamra34@gmail.com>
2024-05-08 14:26:42 +0300

    new main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+#!/bin/python
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/hello/<name>')
-def hello_name(name):
-    return 'Hello %s!' % name
+@app.route('/')
+def hello_name():
+    return 'Hello world!' 
 
-if __name__ == '__main__':
-    app.run()
+app.run(host="0.0.0.0", port=80)
 
